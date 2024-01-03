35dc0ebb318c8e8a29f17be62e0b43022b1046ce
Gabriel Camargos <86752686+Gabrielcamargos28@users.noreply.github.com>
2024-01-03 09:44:25 -0300

    add search by category in rest repository and config web cors

--- back-end/src/main/java/com/ecommerce/springbootecommerce/domain/product/ProductRepository.java
+++ back-end/src/main/java/com/ecommerce/springbootecommerce/domain/product/ProductRepository.java
@@ -1,7 +1,11 @@
 package com.ecommerce.springbootecommerce.domain.product;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-
+    Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
 }
