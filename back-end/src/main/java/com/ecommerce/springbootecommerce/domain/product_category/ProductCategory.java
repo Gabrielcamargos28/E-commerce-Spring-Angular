727bbb5d40180a9c9602074e8c962076f93601c7
Gabriel Camargos <86752686+Gabrielcamargos28@users.noreply.github.com>
2024-01-02 22:22:37 -0300

    create Repository class and RestConfig

--- back-end/src/main/java/com/ecommerce/springbootecommerce/domain/product_category/ProductCategory.java
+++ back-end/src/main/java/com/ecommerce/springbootecommerce/domain/product_category/ProductCategory.java
@@ -15,6 +15,6 @@ public class ProductCategory {
     private Long id;
     @Column(name = "category_name")
     private String categoryName;
-@OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
     private Set<Product> products;
 }
